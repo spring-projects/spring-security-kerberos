9bdb16453518563c2708293199037dd3a4d64c7d
Luke Taylor <luke.taylor@springsource.com>
2009-09-05 14:14:55 +0000

    Tidying imports etc to remove compiler warnings.

--- spring-security-kerberos-core/src/main/java/org/springframework/security/extensions/kerberos/web/SpnegoEntryPoint.java
+++ spring-security-kerberos-core/src/main/java/org/springframework/security/extensions/kerberos/web/SpnegoEntryPoint.java
@@ -21,7 +21,6 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpUtils;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
