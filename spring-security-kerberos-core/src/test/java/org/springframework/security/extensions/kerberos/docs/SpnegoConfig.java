2bd376292155ae33e6fc071e67bf02b80142e2e6
Janne Valkealahti <janne.valkealahti@gmail.com>
2015-03-10 17:40:08 +0000

    Reference doc updates

--- spring-security-kerberos-core/src/test/java/org/springframework/security/extensions/kerberos/docs/SpnegoConfig.java
+++ spring-security-kerberos-core/src/test/java/org/springframework/security/extensions/kerberos/docs/SpnegoConfig.java
@@ -1,6 +1,20 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.security.extensions.kerberos.docs;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
@@ -9,11 +23,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
-import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.extensions.kerberos.KerberosAuthenticationProvider;
 import org.springframework.security.extensions.kerberos.KerberosServiceAuthenticationProvider;
 import org.springframework.security.extensions.kerberos.SunJaasKerberosClient;
@@ -27,93 +36,83 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 @EnableWebMvcSecurity
 public class SpnegoConfig extends WebSecurityConfigurerAdapter {
 
-	@Value("${app.service-principal}")
-	private String servicePrincipal;
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .exceptionHandling()
+                .authenticationEntryPoint(spnegoEntryPoint())
+                .and()
+            .authorizeRequests()
+                .antMatchers("/", "/home").permitAll()
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .loginPage("/login").permitAll()
+                .and()
+            .logout()
+                .permitAll()
+                .and()
+            .addFilterBefore(
+                    spnegoAuthenticationProcessingFilter(authenticationManagerBean()),
+                    BasicAuthenticationFilter.class);
+    }
 
-	@Value("${app.keytab-location}")
-	private String keytabLocation;
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth)
+    		throws Exception {
+        auth
+            .authenticationProvider(kerberosAuthenticationProvider())
+            .authenticationProvider(kerberosServiceAuthenticationProvider());
+    }
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-			.exceptionHandling()
-				.authenticationEntryPoint(spnegoEntryPoint())
-				.and()
-			.authorizeRequests()
-				.antMatchers("/", "/home").permitAll()
-				.anyRequest().authenticated()
-				.and()
-			.formLogin()
-				.loginPage("/login").permitAll()
-				.and()
-			.logout()
-				.permitAll()
-				.and()
-			.addFilterBefore(
-					spnegoAuthenticationProcessingFilter(authenticationManagerBean()),
-					BasicAuthenticationFilter.class);
-	}
+    @Bean
+    public KerberosAuthenticationProvider kerberosAuthenticationProvider() {
+        KerberosAuthenticationProvider provider =
+        		new KerberosAuthenticationProvider();
+        SunJaasKerberosClient client = new SunJaasKerberosClient();
+        client.setDebug(true);
+        provider.setKerberosClient(client);
+        provider.setUserDetailsService(dummyUserDetailsService());
+        return provider;
+    }
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth
-			.authenticationProvider(kerberosAuthenticationProvider())
-			.authenticationProvider(kerberosServiceAuthenticationProvider());
-	}
+    @Bean
+    public SpnegoEntryPoint spnegoEntryPoint() {
+        return new SpnegoEntryPoint("/login");
+    }
 
-	@Bean
-	public KerberosAuthenticationProvider kerberosAuthenticationProvider() {
-		KerberosAuthenticationProvider provider = new KerberosAuthenticationProvider();
-		SunJaasKerberosClient client = new SunJaasKerberosClient();
-		client.setDebug(true);
-		provider.setKerberosClient(client);
-		provider.setUserDetailsService(dummyUserDetailsService());
-		return provider;
-	}
+    @Bean
+    public SpnegoAuthenticationProcessingFilter spnegoAuthenticationProcessingFilter(
+            AuthenticationManager authenticationManager) {
+        SpnegoAuthenticationProcessingFilter filter =
+        		new SpnegoAuthenticationProcessingFilter();
+        filter.setAuthenticationManager(authenticationManager);
+        return filter;
+    }
 
-	@Bean
-	public SpnegoEntryPoint spnegoEntryPoint() {
-		return new SpnegoEntryPoint("/login");
-	}
+    @Bean
+    public KerberosServiceAuthenticationProvider kerberosServiceAuthenticationProvider() {
+        KerberosServiceAuthenticationProvider provider =
+        		new KerberosServiceAuthenticationProvider();
+        provider.setTicketValidator(sunJaasKerberosTicketValidator());
+        provider.setUserDetailsService(dummyUserDetailsService());
+        return provider;
+    }
 
-	@Bean
-	public SpnegoAuthenticationProcessingFilter spnegoAuthenticationProcessingFilter(
-			AuthenticationManager authenticationManager) {
-		SpnegoAuthenticationProcessingFilter filter = new SpnegoAuthenticationProcessingFilter();
-		filter.setAuthenticationManager(authenticationManager);
-		return filter;
-	}
+    @Bean
+    public SunJaasKerberosTicketValidator sunJaasKerberosTicketValidator() {
+        SunJaasKerberosTicketValidator ticketValidator =
+        		new SunJaasKerberosTicketValidator();
+        ticketValidator.setServicePrincipal("HTTP/servicehost.example.org@EXAMPLE.ORG");
+        ticketValidator.setKeyTabLocation(new FileSystemResource("/tmp/service.keytab"));
+        ticketValidator.setDebug(true);
+        return ticketValidator;
+    }
 
-	@Bean
-	public KerberosServiceAuthenticationProvider kerberosServiceAuthenticationProvider() {
-		KerberosServiceAuthenticationProvider provider = new KerberosServiceAuthenticationProvider();
-		provider.setTicketValidator(sunJaasKerberosTicketValidator());
-		provider.setUserDetailsService(dummyUserDetailsService());
-		return provider;
-	}
-
-	@Bean
-	public SunJaasKerberosTicketValidator sunJaasKerberosTicketValidator() {
-		SunJaasKerberosTicketValidator ticketValidator = new SunJaasKerberosTicketValidator();
-		ticketValidator.setServicePrincipal(servicePrincipal);
-		ticketValidator.setKeyTabLocation(new FileSystemResource(keytabLocation));
-		ticketValidator.setDebug(true);
-		return ticketValidator;
-	}
-
-	@Bean
-	public DummyUserDetailsService dummyUserDetailsService() {
-		return new DummyUserDetailsService();
-	}
-
-	static class DummyUserDetailsService implements UserDetailsService {
-
-		public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-			return new User(username, "notUsed", true, true, true, true,
-					AuthorityUtils.createAuthorityList("ROLE_USER"));
-		}
-
-	}
+    @Bean
+    public DummyUserDetailsService dummyUserDetailsService() {
+        return new DummyUserDetailsService();
+    }
 
 }
 //end::snippetA[]
