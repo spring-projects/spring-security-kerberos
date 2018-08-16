1f4b9f95365e03575a7a49ff53abc4ca7a481494
Jannik Upmann <j.upmann@googlemail.com>
2018-08-16 17:39:18 +0200

    fixing package name

--- spring-security-kerberos-client/src/main/java/org/springframework/security/kerberos/client/ldap/KerberosLdapContextSource.java
+++ spring-security-kerberos-client/src/main/java/org/springframework/security/kerberos/client/ldap/KerberosLdapContextSource.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package si.fraport.kerberostest.client.ldap;
+package org.springframework.security.kerberos.client.ldap;
 
 import java.security.PrivilegedAction;
 import java.util.Hashtable;
